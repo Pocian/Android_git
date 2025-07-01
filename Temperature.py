e7831fac92e21ac72a74457587bfee63528aea2d
Posiano Francis <Pocian98@gmail.com>
2025-07-01 18:18:27 +0300

    Temperature Version 2

--- Temperature.py
+++ Temperature.py
@@ -4,3 +4,9 @@ allowed_temp = [temp for temp in Temperatures if temp < 30 and temp >19]
 print(f"During day time, the following temperatures were optimal for maximum motor operation\n {allowed_temp}")
 cost = 35000
 print(f"This will cost us about\n {cost*len(allowed_temp)} in euros")
+if cost*len(allowed_temp) >= 10000:
+  print("This is too much for us")
+  print("Lets stabilize temperature during a day")
+else:
+  print("This is tolerable")
+  
