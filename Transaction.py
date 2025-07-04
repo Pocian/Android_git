0cfd445c207d54820050d79cdbabf0ccba9eddb8
Posiano Francis <Pocian98@gmail.com>
2025-07-04 23:50:12 +0300

    Transaction Version 1

---
+++ Transaction.py
@@ -0,0 +1,23 @@
+income_list = ["1.Salary", "2.Business", "3.Bonus", "4.Tip"]
+expense_list = ["1.Food", "2.Shopping", "3.Transport", "4.Personal expenses"]
+def show_menu():
+  print("-----SIMPLE TRANSACTION ANALYSER-----")
+  print(" ")
+  choices = ["1.Income", "2.Expense"]
+  for i in choices:
+    print(i)
+def user_choice():
+  print(" ")
+  choice =int(input("Choose 1 for income and 2 for Expense"))
+  if choice == 1:
+    print("Welcome to income list")
+    print(" ")
+    for i in income_list:
+      print(i)
+  else:
+    print("Welcome to Expenses list")
+    for i in expense_list:
+      print(i)
+show_menu()
+user_choice()
+  
