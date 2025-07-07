42356141be268c2272e782c834272a8f3516953b
Posiano Francis <Pocian98@gmail.com>
2025-07-07 10:49:31 +0300

    Staircase Version 2

--- Quickref.py
+++ Quickref.py
@@ -1,2 +1,10 @@
-for i in range(1,11):
+steps = 10
+# Left-aligned staircase
+for i in range(1, steps + 1):
     print("*" * i)
+
+print()  # Blank line between the two staircases
+
+# Right-aligned staircase
+for i in range(1, steps + 1):
+    print(" " * (steps - i) + "*" * i)
